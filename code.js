0b1f60fac082e40f9b1adbd50fca27a86642b032
Wall-Dough <awalden0808@gmail.com>
2015-01-01 10:23:27 -0600

    Update code.js

--- code.js
+++ code.js
@@ -940,7 +940,7 @@ window.onload = function () {
 	window.onkeydown = function(e) {
 		var key = e.keyCode ? e.keyCode : e.which;
 		var i = char_codes.indexOf(key);
-		if ((key == 9) || ((key <= 40) && (key >= 37))) {
+		if ((key == 8) || (key == 9) || ((key <= 40) && (key >= 37))) {
 			if (e.preventDefault) {
 				e.preventDefault();
 			}
